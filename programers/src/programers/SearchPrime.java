1c91d439c3d0fef846eb5578e4c4fa52ebbc7137
mezza <gpwl8411@gmail.com>
2020-07-06 02:57:16 +0900

    brute force

--- programers/src/programers/SearchPrime.java
+++ programers/src/programers/SearchPrime.java
@@ -1,72 +1,51 @@
 package programers;
 
 import java.util.HashSet;
+import java.util.stream.Stream;
 
 public class SearchPrime {
 
-	static HashSet<Integer> set = new HashSet<>();
-
+	
+	static HashSet<Integer> ansSet = new HashSet<>();
 	public static void main(String[] args) {
-		String numbers = "131";
-		boolean[] visited = new boolean[numbers.length()];
-		System.out.println("start");
+		String numbers = "131";		
+		int[] intNumbers = Stream.of(numbers.split("")).mapToInt(Integer::parseInt).toArray();
+		
 		for (int i = 1; i <= numbers.length(); i++) {
-				
-				comb(numbers, visited, "", 0, numbers.length(), i, 0);
+				permutation(intNumbers,0,numbers.length(),i);
 		}
-		for(int s : set){
-//			System.out.println(s);
-		}
-		System.out.println(set.size());
+		System.out.println(ansSet.size());
 
 	}
+		
 
-	public static void comb(String numbers, boolean[] visited, String num, int start, int n, int r, int cnt) {
-		if (cnt == n+1)
-			return;
-		if (r == 0) {
-			String reverse = new StringBuilder(num).reverse().toString();
-			int number = Integer.parseInt(num);
-			int revNumber = Integer.parseInt(reverse);
-			if (prime(number)) {
-				set.add(number);
-			}
-			if (prime(revNumber)) {
-				set.add(revNumber);
-			}
-			System.out.println(number);
-			System.out.println(revNumber);
-			return;
-		}
-
-		start %= numbers.length();
-		for (int i = start; i < n; i++) {
-			if (visited[i])
-				continue;
-			String tmp = num;
-			comb(numbers, visited, num, i + 1, n, r, cnt + 1);
-			num += numbers.charAt(i);
-			visited[i] = true;
-			comb(numbers, visited, num, i + 1, n, r - 1, cnt + 1);
-			num = tmp;
-			visited[i] = false;
-		}
-		for (int i = start; i > 0 ; i--) {
-			if (visited[i])
-				continue;
-			String tmp = num;
-			comb(numbers, visited, num, i - 1, n, r, cnt + 1);
-			num += numbers.charAt(i);
-			visited[i] = true;
-			comb(numbers, visited, num, i - 1, n, r - 1, cnt + 1);
-			num = tmp;
-			visited[i] = false;
-		}
-		return;
-
+	static void permutation(int[] arr, int depth, int n, int r) {
+		if (depth == r) {
+			String checkNum = "";
+	    	for (int i = 0; i < r; i++){
+	    		checkNum = checkNum+arr[i];
+	    	}
+	    	if(isPrime(Integer.parseInt(checkNum))){
+    			ansSet.add(Integer.parseInt(checkNum));
+    		}
+	        return ;
+	    }
+	 
+	    for (int i=depth; i<n; i++) {
+	        swap(arr, depth, i);
+	        permutation(arr, depth + 1, n, r);
+	        swap(arr, depth, i);
+	    }
+	    
+	}
+	 
+	static void swap(int[] arr, int depth, int i) {
+	    int temp = arr[depth];
+	    arr[depth] = arr[i];
+	    arr[i] = temp;
 	}
 
-	public static boolean prime(int num) {
+	static boolean isPrime(int num) {
 		if (num == 1 || num == 0)
 			return false;
 		for (int i = 2; i <= Math.sqrt(num); i++) {
