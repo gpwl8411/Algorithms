27f430d146dae528f4ee18b71e2ba3b2b882498f
mezza <gpwl8411@gmail.com>
2020-09-22 04:01:36 +0900

    Ex_16946

--- baekjoon/src/baekjoon/Ex_16946.java
+++ baekjoon/src/baekjoon/Ex_16946.java
@@ -28,12 +28,12 @@ public class Ex_16946 {
 		group = new int[n][m];
 		for(int i=0;i<n;i++) {
 			String[] str = br.readLine().split("");
-//			int j=0;
 			for(int j=0;j<m;j++){
 				arr[i][j]= Integer.parseInt(str[j]);
-//				System.out.print(arr[i][j-1]);
 			}
 		}
+		
+		//각각 그룹마다 다른 key값을 가지게해줌 
 		int groupCnt=1;
 		groupSize = new HashMap<>(); 
 		for(int i=0;i<n;i++) {
@@ -44,7 +44,7 @@ public class Ex_16946 {
 				}
 			}
 		}
-
+		//답을 이중배열로 출력함
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < m; j++) {
@@ -59,10 +59,14 @@ public class Ex_16946 {
 		System.out.println(sb);
 		
 	}
+	//벽에 맞닿은 그룹의 합을 구함
 	static int count(int x,int y) {
 		int cnt=1;
 		if(arr[x][y]==0) return 0;
 		Set<Integer> set = new HashSet<>();
+		
+		//벽에 맞닿은 4방향만 구하면됨
+		//그 방향의 그룹의 0의 갯수 정보는 이미 구했기 때문
 		for(int i=0;i<4;i++) {
 			int[] dx = {0,1,0,-1};
 			int[] dy = {1,0,-1,0};
@@ -72,6 +76,7 @@ public class Ex_16946 {
 			
 			if(sx < 0 || sy < 0 || sx>=n || sy >= m || group[sx][sy]==0)
 				continue;
+			//맞닿은 그룹이 중복일 경우를 위해 set에 저장함
 			set.add(group[sx][sy]);
 			
 		}
@@ -81,9 +86,9 @@ public class Ex_16946 {
 		
 		return cnt%10;
 	}
+	//그룹마다 몇개의 0이 포함되었는지를 리턴해줌
 	static int bfs(int x,int y,int groupCnt) {
 		int cnt=1;
-//		boolean[][] visited = new boolean[n][m];
 		Queue<Point> q = new LinkedList<>();
 		q.add(new Point(x,y));
 		group[x][y]=groupCnt;
@@ -91,16 +96,15 @@ public class Ex_16946 {
 			Point point = q.poll(); 
 			int[] dx = {0,1,0,-1};
 			int[] dy = {1,0,-1,0};
-//			visited[point.x][point.y] = true;
 			for(int i=0;i<4;i++) {
 				int sx = point.x+dx[i];
 				int sy = point.y+dy[i];
 				
 				if(sx < 0 || sy <0 || sx >= n || sy >= m)
 					continue;
+				//아직 그룹에 속하지 않았고 && 벽이 아니라면 카운트해준다.
 				if(group[sx][sy]==0 && arr[sx][sy]==0) {
 					group[sx][sy]=groupCnt;
-//					visited[sx][sy]=true;
 					cnt++;
 					q.add(new Point(sx,sy));
 				}
