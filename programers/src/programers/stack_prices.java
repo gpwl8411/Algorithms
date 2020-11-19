c7bd6ed22c5506b5f5cadd6bdaf9238389966d6e
mezza <gpwl8411@gmail.com>
2020-11-19 23:12:27 +0900

    Stack_prices

--- programers/src/programers/stack_prices.java
+++ programers/src/programers/stack_prices.java
@@ -7,44 +7,20 @@ public class stack_prices {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] prices = {2,2,2,1,2};
-		int[] answer= new int[prices.length];
-		Stack<Integer> stack = new Stack<Integer>();
-		int day=0;
-		for(int i=0;i<prices.length;i++){
-			int p=0;
-			stack.push(prices[i]);		
-			day++;
-			if(i!=prices.length-1 && i!=prices.length-2){
-//				System.out.println(stack.peek()+"-"+prices[i+1]);
-//				System.out.println(stack.peek() > prices[i+1]);
-				while(!stack.isEmpty() && stack.peek() > prices[i+1]){
-//					System.out.println("pop"+stack.pop());
-					stack.pop();
-//					System.out.println(i+":"+(i-p)+":"+(p+1));
-					answer[i-p]=p+1;
-					p++;
-					//System.out.println(answer[i-p]+":"+p);
-//					System.out.println(i+":"+p);
-				}
-			}
-			
-		}
-		int j=prices.length-1;
-		while(!stack.isEmpty()){
-			if(answer[j]!=0){
-				j--;
-				day--;
-				continue;
-			}
-			int a = stack.pop();
-			answer[j]=prices.length-day;
-//			System.out.println(a+";"+answer[j]);
-			day--;
-			j--;
-		}
-		for(int i=0;i<prices.length;i++){
-			System.out.println(answer[i]);
-		}
+		int[] answer = new int[prices.length];
+        
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = i+1; j < answer.length; j++) {
+                if (prices[i] > prices[j]) {
+                    answer[i] = j-i;
+                    break;
+                }
+                if (j==answer.length-1) answer[i] = j-i;
+            }
+        }
+        for(int i : answer) {
+        	System.out.println(i);
+        }
 	}
 
 }
