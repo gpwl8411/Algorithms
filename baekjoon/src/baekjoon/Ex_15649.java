8687dff8ee5c06b2895d8c613417006d2f7223af
gpwl8411 <gpwl8411@gmail.com>
2021-04-12 18:40:59 +0900

    Ex_15649

---
+++ baekjoon/src/baekjoon/Ex_15649.java
@@ -0,0 +1,46 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex_15649 {
+
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		//백트래킹 N과 M (1)
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		permutation(new int[n],new boolean[n],n,m,0);
+		
+		System.out.println(sb.toString());
+		
+	}
+	
+	static void permutation(int[] arr, boolean[] visited, int n, int r, int depth) {
+		if(depth==r) {
+			for(int i=0;i<r;i++) {
+				sb.append(arr[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i=0; i<n; i++) {
+	        if (visited[i] != true) {
+	            visited[i] = true;
+	            arr[depth] = i+1;
+	            permutation(arr, visited, n, r,depth + 1); 
+	            visited[i] = false;;
+	        }
+	    }
+			
+	}
+
+}
