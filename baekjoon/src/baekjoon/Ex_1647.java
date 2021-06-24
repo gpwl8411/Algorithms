5fd14811c47a261f3a3ef0c8ea9c29bb28508658
gpwl8411 <gpwl8411@gmail.com>
2021-06-24 23:13:24 +0900

    Ex_1647_kruskal

--- baekjoon/src/baekjoon/Ex_1647.java
+++ baekjoon/src/baekjoon/Ex_1647.java
@@ -11,11 +11,13 @@ import java.util.StringTokenizer;
 
 public class Ex_1647 {
 
-	static List<Node>[] list;
-	static boolean[] visited;
-	static int ans = 0;
+//	static List<Node>[] list;
+//	static boolean[] visited;
+	static int ans = 0,cnt=0;
 	static int n;
-	static int max = Integer.MIN_VALUE;
+//	static int max = Integer.MIN_VALUE;
+	static int[] parent;
+	static PriorityQueue<Node> pq = new PriorityQueue<>();
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
@@ -24,67 +26,112 @@ public class Ex_1647 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		list = new List[n + 1];
-		visited = new boolean[n + 1];
-
-		for (int i = 0; i <= n; i++)
-			list[i] = new ArrayList<>();
-
+//		list = new List[n + 1];
+//		visited = new boolean[n + 1];
+		parent = new int[n+1];
+//		for (int i = 0; i <= n; i++)
+//			list[i] = new ArrayList<>();
+		
+		for(int i=1;i<=n;i++)
+			parent[i] = i;
+			
 		while (m-- > 0) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 
-			list[a].add(new Node(b, w));
-			list[b].add(new Node(a, w));
+//			list[a].add(new Node(b, w));
+//			list[b].add(new Node(a, w));
+			pq.add(new Node(a,b,w));
 		}
-		mts();
-		System.out.println(ans-max);
+//		prim();
+		kruskal();
+		System.out.println(ans);
 	}
-
-	static void mts() {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		Queue<Integer> nodeQ = new LinkedList<>();
-		nodeQ.add(1);
-		List<Node> curList;
-
-		while (!nodeQ.isEmpty()) {
-			int cur = nodeQ.poll();
-			visited[cur] = true;
-			curList = list[cur];
-			for (int i = 0; i < curList.size(); i++) {
-				if (!visited[curList.get(i).end]) {
-					pq.add(curList.get(i));
-				}
-			}
-			while (!pq.isEmpty()) {
-				Node tmpNode = pq.poll();
-				if (!visited[tmpNode.end]) {
-					visited[tmpNode.end] = true;
-					ans += tmpNode.w;
-					max = Math.max(tmpNode.w, max);
-					nodeQ.add(tmpNode.end);
-					break;
-				}
+	static int find(int x) {
+		if(parent[x]==x)
+			return x;
+		else 
+			return parent[x] = find(parent[x]);
+	}
+	static void union(int x,int y) {
+		x = find(x);
+		y = find(y);
+		
+		if(x<y)
+			parent[y] = x;
+		else
+			parent[x] = y;
+	}
+	static void kruskal(){
+		while(!pq.isEmpty()&&cnt < n-2) {
+			Node cur = pq.poll();
+			if(find(cur.x)!=find(cur.y)) {
+				cnt++;
+				union(cur.x,cur.y);
+				ans+=cur.w;
+//				max = Math.max(max, cur.w);
 			}
 		}
-
 	}
-
-	static class Node implements Comparable<Node> {
-		int end;
+	static class Node implements Comparable<Node>{
+		int x;
+		int y;
 		int w;
-
-		Node(int end, int w) {
-			this.end = end;
+		Node(int x,int y,int w){
+			this.x = x;
+			this.y = y;
 			this.w = w;
 		}
-
+		
 		@Override
 		public int compareTo(Node n) {
-			return w - n.w;
+			return w-n.w;
 		}
 	}
+//	static void prim() {
+//		PriorityQueue<Node> pq = new PriorityQueue<>();
+//		Queue<Integer> nodeQ = new LinkedList<>();
+//		nodeQ.add(1);
+//		List<Node> curList;
+//
+//		while (!nodeQ.isEmpty()) {
+//			int cur = nodeQ.poll();
+//			visited[cur] = true;
+//			curList = list[cur];
+//			for (int i = 0; i < curList.size(); i++) {
+//				if (!visited[curList.get(i).end]) {
+//					pq.add(curList.get(i));
+//				}
+//			}
+//			while (!pq.isEmpty()) {
+//				Node tmpNode = pq.poll();
+//				if (!visited[tmpNode.end]) {
+//					visited[tmpNode.end] = true;
+//					ans += tmpNode.w;
+//					max = Math.max(tmpNode.w, max);
+//					nodeQ.add(tmpNode.end);
+//					break;
+//				}
+//			}
+//		}
+//
+//	}
+
+//	static class Node implements Comparable<Node> {
+//		int end;
+//		int w;
+//
+//		Node(int end, int w) {
+//			this.end = end;
+//			this.w = w;
+//		}
+//
+//		@Override
+//		public int compareTo(Node n) {
+//			return w - n.w;
+//		}
+//	}
 
 }
